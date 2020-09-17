ebf1e24869ec8f29956e7bb1b4540715232a2259
Jai Tan <tantzhang1@gmail.com>
2020-09-17 20:13:43 +0800

    Adding new function

--- assets/js/GetWeather.js
+++ assets/js/GetWeather.js
@@ -222,6 +222,40 @@ const showOutput = (index) => {
     btnNext.disabled = isIndexOuterEdge(index, FThours.length, viewSize);
 }
 
-startTime();
-GetData();
+// startTime();
+// GetData();
+
+const openModalBotton = document.querySelectorAll('[data-modal-target]')
+const closeModalBotton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalBotton.forEach(button =>{
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+overlay.addEventListener('click',() =>{
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+closeModalBotton.forEach(button =>{
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
 
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
   process.env.GOOGLE_API_KEY = 'AIzaSyAw9PVACjlLl2HtKdUwxBw0DGhyKwpK9pQ';
-  window = new BrowserWindow({ width: 950, height: 600 })
+  window = new BrowserWindow({ width: 1200, height: 800 })
   window.loadFile('index.html')
 }
 
