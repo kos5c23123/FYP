7bb00abd321314b57608753a7f23cf4112238a7f
DannyTan <tantszhang1@gmail.com>
2021-04-17 14:11:30 +0800

    updated

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const { app, BrowserWindow, globalShortcut } = require('electron')
 
 function createWindow() {
     process.env.GOOGLE_API_KEY = 'AIzaSyDNgedYtU-GR7aPCGcZUHCjtMKd7uomw1c';
-    window = new BrowserWindow({ width: 1024, height: 600 })
-    // window = new BrowserWindow()
-    // window.setFullScreen(true)
+    // window = new BrowserWindow({ width: 1024, height: 600 })
+    window = new BrowserWindow()
+    window.setFullScreen(true)
     // window.webContents.openDevTools()
     window.loadFile('index.html')
     globalShortcut.register('ESC', () => {
